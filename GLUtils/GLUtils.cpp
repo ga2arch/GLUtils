c2820f5df68ff50b2a5122d6943c4ce197e7e22e
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-12-02 22:51:24 +0100

    fixed normals, texture lighting

--- GLUtils/GLUtils.cpp
+++ GLUtils/GLUtils.cpp
@@ -203,11 +203,13 @@ GLuint GLUtils::load_texture(const std::string& filename) {
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height,
                  0, GL_RGBA, GL_UNSIGNED_BYTE, image.data());
     
+    //When texture area is small, bilinear filter the closest mipmap
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    
+    //When large, bilinear filter the FIRST mipmap
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 //    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 //    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); //GL_NEAREST = no smoothing
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
   
     return texture;
 }
